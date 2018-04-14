0858e9e4c20181a8684a75ac943845e5d8b821a0
sergevs <abricus@yandex.ru>
2018-04-14 22:40:47 +0300

    tests added

---
+++ Cinder/Queens/test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from cinder import objects
+objects.register_all()
+
+from oslo_config import cfg
+from oslo_config import types
+from cinder.volume import configuration
+from cinder import context
+
+from cinder.volume.drivers.huawei.huawei_driver import HuaweiFCDriver
+
+
+def dump(obj):
+  for attr in dir(obj):
+    print("obj.%s = %r" % (attr, getattr(obj, attr)))
+
+c=configuration.DefaultGroupConfiguration()
+d = HuaweiFCDriver(configuration=c)
+#ctx=context.RequestContext()
+#dump(d.configuration)
+
+d.do_setup(None)
